1b8d4895a2ece17ab12f2c8d2eb89544c0592aa8
veerababu0705-cell <veerababu0705@gmail.com>
2026-07-06 22:42:51 +0530

    Updated changes

--- components/Background.tsx
+++ components/Background.tsx
@@ -3,12 +3,8 @@
 export default function Background() {
   return (
     <div className="fixed-bg">
-      <div className="stars"></div>
-      <div className="stars2"></div>
-      <div className="stars3"></div>
-      <video autoPlay muted loop playsInline className="video-bg">
-        <source src="/assets/animation video.mp4" type="video/mp4" />
-      </video>
+      <div className="paper-texture"></div>
+      <div className="paper-crease"></div>
     </div>
   )
 }
